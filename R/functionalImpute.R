79a232ec26fab7313172c40fc89e8723e373fea1
Lukasz Kidzinski <lukasz.kidzinski@gmail.com>
2017-03-09 00:10:17 -0800

    Comparison of fpca and fimpute

--- R/functionalImpute.R
+++ R/functionalImpute.R
@@ -11,7 +11,11 @@ functionalImpute = function(Y, basis, K = NULL, maxIter = 100){
   for (i in 1:maxIter){
     Yfill[ynas] = Yhat[ynas]
     Ysvd = svd(Yfill %*% basis)
-    Yhat = Ysvd$u[,dims] %*% (Ysvd$d[dims] * t(Ysvd$v[,dims])) %*% t(basis)
+    Yhat.new = Ysvd$u[,dims] %*% (Ysvd$d[dims] * t(Ysvd$v[,dims])) %*% t(basis)
+    ratio = norm(Yhat.new - Yhat,"F") / norm(Yhat,type = "F")
+    # if (ratio < 1e-3)
+    #   break
+    Yhat = Yhat.new
   }
   Yhat
 }
--- demo/simulation.R
+++ demo/simulation.R
@@ -1,37 +1,77 @@
-library("devtools")
-# devtools::install(".",reload = TRUE)
-# library("fcomplete")
-library("fda")
-library("clusterGeneration")
+# library("devtools")
+# devtools::install_git("https://github.com/kidzik/fpca.git",reload = TRUE)
+# devtools::install(".")
 
-d = 21
-dgrid = 100
+library("fpca")
+library("fcomplete")
+library("fda")
+
+d = 7
+K = 2
+dgrid = 501
 
 # Set up basis
 basis = fda::create.fourier.basis(c(0,1), d)
 #basis = fda::create.bspline.basis(c(0,1), d, 4)
 S = fda::eval.basis(evalarg = 0:(dgrid-1)/(dgrid-1), basisobj = basis) / sqrt(dgrid)
 
-# Generate data
-Sigma = genPositiveDefMat(d)$Sigma
-Ycoef = mvrnorm(n = 1000, Sigma, mu = (d:1 / 3) + rnorm(d))
+# Generate some cool positive definite matrix
+V = svd(matrix(rnorm(d*d),d))$v
+D = diag(c(1,0.9,0.5,exp(-(3:(d-1))))) * 500
+Sigma = V %*% D %*% t(V)
+Ycoef = mvrnorm(n = 1000, Sigma = Sigma, mu = rep(0,d))
 Ytrue = Ycoef %*% t(S)
 
 # Remove 90% of points
 nel = prod(dim(Ytrue))
-nna = ceiling(nel*0.95)
+nna = ceiling(nel*0.99)
 SigmaBig = genPositiveDefMat(dgrid)$Sigma
-noise = mvrnorm(n = 1000, SigmaBig, mu = rep(0,dgrid)) * 0.01
+noise = mvrnorm(n = 1000, SigmaBig, mu = rep(0,dgrid)) * 0.5
 Ynoise = Ytrue+noise
 Y = Ynoise
+par(mfrow=c(1,1))
+matplot(t(Ytrue)[,1:10],t='l')
+matplot(t(Y)[,1:10],t='l')
 Y[sample(nel)[1:nna]] = NA
 
 # Impute functions
-Yhat = functionalImpute(Y, S, 3)
+Yhat = functionalImpute(Y, S, 3, maxIter = 250)
+mean((Ytrue[idx,] - (Yhat[idx,]))^2) / mean(Ytrue^2)
+matplot(t(Yhat)[,1:10],t='l')
 
 # See unbelievable results
-par(mfrow=c(2,2))
-matplot(t(Ytrue)[,7:10],t='l')
-matplot(t(Ynoise)[,7:10],t='l')
-matplot(t(Y)[,7:10],t='p',pch=20)
-matplot(t(Yhat)[,7:10],t='l')
+
+nonna = which(!is.na(Y),arr.ind = T)
+nonna = nonna[order(nonna[,1]),]
+vals = Y[nonna]
+R = cbind(nonna, vals)
+R[,2] = R[,2] / dgrid
+R = R[,c(1,3,2)]
+
+# model and predict
+M = d + 2
+K = 2
+model = fpca.mle(R, M, K, ini.method="EM")
+fpcs = fpca.score(R, model$grid, model$fitted_mean, model$eigenvalues, model$eigenfunctions, model$error_var, K)
+pred = fpca.pred(fpcs, model$fitted_mean, model$eigenfunctions)
+
+# plot results
+matplot(pred, t='l')
+idx = unique(R[,1])
+Ycmp = Y[idx,]
+
+tocmp = !is.na(Ycmp)
+mean((Ycmp[tocmp] - t(pred)[tocmp])^2) / mean((Ycmp[tocmp])^2)
+mean((Ycmp[tocmp] - (Yhat[idx,])[tocmp])^2) / mean((Ycmp[tocmp])^2)
+
+mean((Ytrue[idx,] - t(pred))^2) / mean(Ytrue^2)
+mean((Ytrue[idx,] - (Yhat[idx,]))^2) / mean(Ytrue^2)
+
+# Plots
+par(mfrow=c(2,3))
+matplot(t(Ytrue[idx,])[,1:10],t='l')
+matplot(t(Ynoise[idx,])[,1:10],t='l')
+matplot(t(Y[idx,])[,1:10],t='p',pch=20)
+matplot(t(Yhat[idx,])[,1:10],t='l')
+matplot((pred)[,1:10],t='l')
+frame()
